ec9f049867cf804abc7dffac59f1789bc55118fa
Jonas Esser <jonas@esser.dk>
2026-02-15 16:49:03 +0100

    feat: API /fllws/ endpoint
    Implemented fllws endpoint in minimal API.
    Authorisation is not specified. According to ChatGPT, this should be handled elsewhere, not defined in the swagger endpoints.

--- itu-minitwit/src/MiniTwit.Web/Api.cs
+++ itu-minitwit/src/MiniTwit.Web/Api.cs
@@ -1,13 +1,36 @@
 using Chirp.Infrastructure.Chirp.Repositories;
 using Chirp.Infrastructure.DataTransferObjects;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Chirp.Web;
 
 public static class Api
 {
+    public record FollowRequest(string? Follow, string? Unfollow);
     public static void MapProductEndpoints(this WebApplication app)
     {
-        app.MapGet("/fllws/{username}", 
-            (string username, IFollowRepository followRepository) => followRepository.GetFollowed(username));
+        app.MapGet("/fllws/{username}",
+            (string username,[FromQuery (Name = "latest")] int? latests,[FromQuery (Name = "no")] int? no, IFollowRepository followRepository) =>
+            {
+                return followRepository.GetFollowed(username);
+            });
+        
+        app.MapPost("/fllws/{username}",
+            (string username, [FromQuery (Name = "latest")] int? latests ,[FromBody] FollowRequest request, IFollowRepository followRepository) =>
+            {
+                if (!string.IsNullOrEmpty(request.Follow))
+                {
+                    followRepository.AddFollowing(username,request.Follow);
+                    return Results.Ok($"Following {request.Follow}");
+                }
+
+                if (!string.IsNullOrEmpty(request.Unfollow))
+                {
+                    followRepository.RemoveFollowing(username,request.Unfollow);
+                    return Results.Ok($"Unfollowing {request.Unfollow}");
+                }
+                
+                return Results.BadRequest("Must specify either 'Follow' or 'Unfollow'");
+            });
     }
 }
